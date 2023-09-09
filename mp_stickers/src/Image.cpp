1aa064caacee4c9d4f13f941068a455f73b4b1aa
FPynk <acloh2@illinois.edu>
2023-09-09 21:19:18 +0000

    Implemented desaturate in image

--- mp_stickers/src/Image.cpp
+++ mp_stickers/src/Image.cpp
@@ -122,15 +122,15 @@ void Image::saturate(double amount) {
 
 void Image::desaturate() {
     // Cycle thru each pixel
-    // decrease pixel luminance by 0.1
+    // decrease pixel saturation by 0.1
     unsigned int width = PNG::width();
     unsigned int height = PNG::height();
     for (unsigned int row = 0; row < height; row++) {
         for (unsigned int col = 0; col < width; col++) {
-            if (PNG::getPixel(col, row).l - 0.1 >= 0.0) {
-                PNG::getPixel(col, row).l -= 0.1;
+            if (PNG::getPixel(col, row).s - 0.1 >= 0.0) {
+                PNG::getPixel(col, row).s -= 0.1;
             } else {
-                PNG::getPixel(col, row).l = 0;
+                PNG::getPixel(col, row).s = 0;
             }
         }
     }
@@ -139,7 +139,19 @@ void Image::desaturate() {
 // Desaturates an Image by decreasing the saturation of every pixel by amount.
 // This function ensures that the saturation remains in the range [0, 1].
 void Image::desaturate(double amount) {
-
+    // Cycle thru each pixel
+    // decrease pixel saturation by 0.1
+    unsigned int width = PNG::width();
+    unsigned int height = PNG::height();
+    for (unsigned int row = 0; row < height; row++) {
+        for (unsigned int col = 0; col < width; col++) {
+            if (PNG::getPixel(col, row).s - amount >= 0.0) {
+                PNG::getPixel(col, row).s -= amount;
+            } else {
+                PNG::getPixel(col, row).s = 0;
+            }
+        }
+    }
 }
 
 void Image::grayscale() {
