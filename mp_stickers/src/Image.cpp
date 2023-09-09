9cb441e1c6f5469c2b84817701d996537717eaa9
FPynk <acloh2@illinois.edu>
2023-09-09 07:33:15 +0000

    Finalised lighten and added darken

--- mp_stickers/src/Image.cpp
+++ mp_stickers/src/Image.cpp
@@ -18,6 +18,8 @@ void Image::lighten() {
             // Increase pixel luminance by 0.1
             if (PNG::getPixel(col, row).l + 0.1 <= 1.0) {
                 PNG::getPixel(col, row).l += 0.1;
+            } else {
+                PNG::getPixel(col, row).l = 1;
             }
         }
     }
@@ -34,6 +36,8 @@ void Image::lighten(double amount) {
             // Increase pixel luminance by 0.1
             if (PNG::getPixel(col, row).l + amount <= 1.0) {
                 PNG::getPixel(col, row).l += amount;
+            } else {
+                PNG::getPixel(col, row).l = 1;
             }
         }
     }
@@ -42,10 +46,29 @@ void Image::lighten(double amount) {
 void Image::darker() {
     // Cycle thru each pixel
     // decrease pixel luminance by 0.1
+    unsigned int width = PNG::width();
+    unsigned int height = PNG::height();
+    for (unsigned int row = 0; row < height; row++) {
+        for (unsigned int col = 0; col < width; col++) {
+            if (PNG::getPixel(col, row).l - 0.1 >= 0.0) {
+                PNG::getPixel(col, row).l -= 0.1;
+            }
+        }
+    }
 }
 
 void Image::darken(double amount) {
-
+    // Cycle thru each pixel
+    // decrease pixel luminance by 0.1
+    unsigned int width = PNG::width();
+    unsigned int height = PNG::height();
+    for (unsigned int row = 0; row < height; row++) {
+        for (unsigned int col = 0; col < width; col++) {
+            if (PNG::getPixel(col, row).l - amount >= 0.0) {
+                PNG::getPixel(col, row).l -= amount;
+            }
+        }
+    }
 }
 void Image::saturate() {
 
