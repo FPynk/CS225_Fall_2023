4ccf39e4c302ed6ca796a07c27901e0d76bec5ab
FPynk <acloh2@illinois.edu>
2023-09-10 03:27:48 +0000

    StickerSheet Base setup

---
+++ mp_stickers/src/StickerSheet.cpp
@@ -0,0 +1,36 @@
+#include "StickerSheet.h"
+
+StickerSheet(const Image &picture, unsigned max) {
+  
+}
+
+StickerSheet(const StickerSheet &other) {
+
+}
+const StickerSheet& operator=(const StickerSheet &other) {
+
+}
+void changeMaxStickers(unsigned max) {
+
+}
+int addSticker(Image& sticker, int x, int y) {
+
+}
+int setStickerAtLayer(Image &sticker, int x, int y) {
+
+}
+bool translate(unsigned index, int x, int y) {
+
+}
+void 	removeSticker (unsigned index) {
+
+}
+Image* getSticker(unsigned index) {
+
+}
+int layers() const {
+
+}
+Image render() const {
+
+}
--- mp_stickers/src/StickerSheet.h
+++ mp_stickers/src/StickerSheet.h
@@ -4,3 +4,24 @@
  */
 #pragma once
 
+#include "Image.h"
+#include <vector>
+
+class StickerSheet {
+    public:
+        StickerSheet(const Image &picture, unsigned max);
+        StickerSheet(const StickerSheet &other);
+        const StickerSheet& operator=(const StickerSheet &other);
+        void changeMaxStickers(unsigned max);
+        int addSticker(Image& sticker, int x, int y);
+        int setStickerAtLayer(Image &sticker, int x, int y);
+        bool translate(unsigned index, int x, int y);
+        void 	removeSticker (unsigned index);
+        Image* getSticker(unsigned index);
+        int layers() const;
+        Image render() const;
+    private:
+        Image* base_;
+        std::vector<Image> stickers_;
+        unsigned int max;
+};
