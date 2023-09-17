9d6e651b2d5298111bbf2b1ce635d3e220eb6f2c
FPynk <acloh2@illinois.edu>
2023-09-17 03:15:36 +0000

    added more tests for iterators

--- mp_lists/entry/lists.cpp
+++ mp_lists/entry/lists.cpp
@@ -12,6 +12,25 @@ int main() {
     test_int.insertBack(5);
     test_int.insertBack(6);
 
+    std::cout << "List<int> contents: ";
+    for (auto it = test_int.begin(); it != test_int.end(); ++it) {
+        std::cout << *it << " ";
+    }
+    std::cout << std::endl;
+
+    test_char.insertFront('c');
+    test_char.insertFront('b');
+    test_char.insertFront('a');
+    test_char.insertBack('d');
+    test_char.insertBack('e');
+    test_char.insertBack('f');
+
+    std::cout << "List<char> contents: ";
+    for (auto it = test_char.begin(); it != test_char.end(); ++it) {
+        std::cout << *it << " ";
+    }
+    std::cout << std::endl;
+
     std::cout << "Success!" << std::endl;
     return 0;
 }
