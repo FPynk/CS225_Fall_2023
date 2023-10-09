6282f7a0e8328d4b9f4e27d1936dd67398fa5a00
FPynk <acloh2@illinois.edu>
2023-10-09 22:20:47 +0000

    maptiles uses maps now

--- mp_mosaics/src/maptiles.cpp
+++ mp_mosaics/src/maptiles.cpp
@@ -29,31 +29,31 @@ MosaicCanvas* mapTiles(SourceImage const& theSource,
     }
     // Output of MosaicCanvas*
     MosaicCanvas* output = new MosaicCanvas(theSource.getRows(), theSource.getColumns());
-    // Convert vector of tileimages to vector of points
+    // Convert vector of tileimages to vector of points and make a map
     vector<Point<3>> thePoints;
-    for (TileImage TI : theTiles) {
+    map<Point<3>, TileImage*> pointToTileMap;
+    for (TileImage& TI : theTiles) {
         LUVAPixel colour = TI.getAverageColor();
         Point<3> point =  convertToXYZ(colour);
         thePoints.push_back(point);
+        pointToTileMap[point] = &TI;
     }
     // construct kd tree of points
     KDTree<3> point_tree = KDTree(thePoints);
 
     // Go through SourceImage and match to closest TileImage
-    for (int row = 0; row < theSource.getRows(); row++) {
-        for (int col = 0; col < theSource.getColumns(); col++) {
+    for (int row = 0; row < theSource.getRows(); ++row) {
+        for (int col = 0; col < theSource.getColumns(); ++col) {
             // convert theSource luva into a point
             LUVAPixel colour = theSource.getRegionColor(row, col);
             Point<3> point =  convertToXYZ(colour);
             
             // Finding closest TileImage using find nearest neighbour
             Point<3> closest_point =  point_tree.findNearestNeighbor(point);
-            auto it = find(thePoints.begin(), thePoints.end(), closest_point);
-            int pos = distance(thePoints.begin(), it);
-            TileImage closest_tile = theTiles[pos];
+            TileImage* closest_tile = pointToTileMap[closest_point];
 
             // set tile in mosaic
-            output->setTile(row, col, &closest_tile);
+            output->setTile(row, col, closest_tile);
         }
     }
     return output;
